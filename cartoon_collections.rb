69964fc8d835687f3c710eb39933a6f52179da7c
Jack Overby <jroverby92@gmail.com>
2020-01-14 00:24:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,8 +2,8 @@ def roll_call_dwarves(ar)
   ar.each_with_index{|ho, ix| puts "#{ix+1}. #{ho}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(ar)
+  ar.map{|x| 
 end
 
 def long_planeteer_calls# code an argument here
